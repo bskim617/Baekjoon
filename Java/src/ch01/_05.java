ab598a2290dc5fa1b6474e484f7f5667fc3d00a6
bskim617 <bskim617@gmail.com>
2023-05-25 19:55:52 +0900

    백준 문제 2023-05-25

---
+++ Java/src/ch01/_05.java
@@ -0,0 +1,13 @@
+package ch01;
+
+import java.util.Scanner;
+
+public class _05 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Double num1 = sc.nextDouble();
+		Double num2 = sc.nextDouble();
+		
+		System.out.println(num1 / num2);
+	}
+}
